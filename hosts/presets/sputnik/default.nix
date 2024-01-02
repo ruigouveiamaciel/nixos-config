073c69ece4eb0e132c3fbcd6a1e21788bf31fe34
Rui Maciel <ruigouveiamaciel@proton.me>
2024-01-02 20:07:23 +0000

    port forward ssh to host when in vm

--- hosts/presets/sputnik/default.nix
+++ hosts/presets/sputnik/default.nix
@@ -18,6 +18,23 @@
     hostName = "sputnik";
   };
 
+  virtualisation.vmVariant.virtualisation.forwardPorts = [
+    {
+      from = "host";
+      proto = "tcp";
+      host.port = 2222;
+      host.address = "0.0.0.0";
+      guest.port = 22;
+      guest.address = "10.0.2.15";
+    }
+  ];
+
+  virtualisation.vmVariant.services.openssh = {
+    extraConfig = ''
+      ListenAddress 10.0.2.15
+    '';
+  };
+  
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
