d2710aca7d237ef41718a50684390ab9867481e3
Rui Maciel <ruigouveiamaciel@proton.me>
2024-01-17 20:26:29 +0000

    more attempts at sudo

--- hosts/configs/sputnik/default.nix
+++ hosts/configs/sputnik/default.nix
@@ -25,7 +25,7 @@
     pam.services.sudo = { config, ... }: {
       sshAgentAuth = true;
       rules.auth.rssh = {
-        order = config.rules.auth.unix.order - 10;
+        order = 10500;
         control = "sufficient";
         modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
       };
