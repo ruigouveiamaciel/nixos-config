fb6df5cbc8dfc66b74f0040f10f2491c573273ac
Rui Maciel <ruigouveiamaciel@proton.me>
2024-01-17 21:47:33 +0000

    ssh agent forwarding

--- users/rui/home-manager/features/cli/gpg.nix
+++ users/rui/home-manager/features/cli/gpg.nix
@@ -27,7 +27,19 @@ in {
     enableExtraSocket = true;
   };
 
-  programs = {
+  programs = let
+      fixGpg = /* bash */ ''
+        gpgconf --launch gpg-agent
+      '';
+    in {
+      # Start gpg-agent if it's not running or tunneled in
+      # SSH does not start it automatically, so this is needed to avoid having to use a gpg command at startup
+      # https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart
+      bash.profileExtra = fixGpg;
+      fish.loginShellInit = fixGpg;
+      zsh.loginExtra = fixGpg;
+
+
     gpg = {
       enable = true;
       settings = {
@@ -41,8 +53,21 @@ in {
       ];
     };
   };
-
-  #programs.fish.shellInit = ''
-  #  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  #'';
+  
+  systemd.user.services = {
+    # Link /run/user/$UID/gnupg to ~/.gnupg-sockets
+    # So that SSH config does not have to know the UID
+    link-gnupg-sockets = {
+      Unit = {
+        Description = "link gnupg sockets from /run to /home";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.coreutils}/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets";
+        ExecStop = "${pkgs.coreutils}/bin/rm $HOME/.gnupg-sockets";
+        RemainAfterExit = true;
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
+  };
 }
--- users/rui/home-manager/features/cli/ssh.nix
+++ users/rui/home-manager/features/cli/ssh.nix
@@ -9,10 +9,25 @@
 in {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host sputnik.maciel.sh
-        ProxyCommand ${pkgs.unstable.cloudflared}/bin/cloudflared access ssh --hostname %h
-    '';
+    matchBlocks = {
+      net = {
+        host = "192.168.1.76";
+        forwardAgent = true;
+        remoteForwards = [{
+          bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
+          host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
+        }];
+      };
+      cloudd = {
+        host = "sputnik.maciel.sh";
+        proxyCommand = "${pkgs.unstable.cloudflared}/bin/cloudflared access ssh --hostname %h";
+        forwardAgent = true;
+        remoteForwards = [{
+          bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
+          host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
+        }];
+      };
+    };
   };
 
   home.persistence."/nix/persist${config.home.homeDirectory}" = {
