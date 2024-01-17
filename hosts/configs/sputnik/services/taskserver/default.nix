29bb0cfa5a759af06bbc40dc1ff72ce95d393c8a
Rui Maciel <ruigouveiamaciel@proton.me>
2024-01-17 14:44:43 +0000

    persistent data dir for taskserver

--- hosts/configs/sputnik/services/taskserver/default.nix
+++ hosts/configs/sputnik/services/taskserver/default.nix
@@ -4,6 +4,7 @@
     openFirewall = true;
     fqdn = "taskserver.maciel.sh";
     listenHost = "192.168.1.76";
+    dataDir = "/nix/backup/services/taskserver";
     pki.manual = {
       server.key = config.sops.secrets.taskwarrior-server-key.path;
       server.crl = config.sops.secrets.taskwarrior-server-crl.path;
