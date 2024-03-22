fbae000ca27661c99ba129ef06ef3a87aa0f3b13
Rui Maciel <ruigouveiamaciel@proton.me>
2024-03-22 18:18:48 +0000

    added nine server to ssh config

--- users/rui/home-manager/features/cli/ssh.nix
+++ users/rui/home-manager/features/cli/ssh.nix
@@ -21,6 +21,17 @@ in {
           }
         ];
       };
+      nine = {
+        host = "ssh.ninegaming.pt";
+        proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h";
+        forwardAgent = true;
+        remoteForwards = [
+          {
+            bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
+            host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
+          }
+        ];
+      };
     };
   };
 }
