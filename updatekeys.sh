57ca306560fb5b54e603a11a274564ef631b69d2
Rui Maciel <ruigouveiamaciel@proton.me>
2025-03-19 18:49:36 +0000

    update secrets

--- updatekeys.sh
+++ updatekeys.sh
@@ -3,3 +3,4 @@
 sops updatekeys -y ./hosts/proxmox/devbox/secrets.yaml
 sops updatekeys -y ./hosts/proxmox/homepage/secrets.yaml
 sops updatekeys -y ./hosts/proxmox/media-server/secrets.yaml
+sops updatekeys -y ./hosts/proxmox/storagebox/secrets.yaml
