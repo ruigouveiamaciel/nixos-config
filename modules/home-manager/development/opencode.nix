7c2d9d2d7bcf418f9e06dd9991e01ef6da39184d
Rui Maciel <ruigouveiamaciel@proton.me>
2025-10-27 11:21:42 +0000

    fix persistence for opencode

--- modules/home-manager/development/opencode.nix
+++ modules/home-manager/development/opencode.nix
@@ -1,33 +1,34 @@
 {
   pkgs,
   lib,
-  config,
+  options,
   ...
 }: {
-  config = lib.mkMerge [
-    {
-      home = {
-        packages = with pkgs; [
-          unstable.opencode
-        ];
-
-        sessionVariables = {
-          OPENCODE_CONFIG = ./opencode.json;
-        };
-      };
-    }
-
-    (lib.mkIf (builtins.hasAttr "persistence" config.home) {
-      home.persistence = {
-        "/persist" = {
-          directories = [
-            ".config/opencode"
-            ".cache/opencode"
-            ".local/share/opencode"
-            ".local/state/opencode"
+  config = lib.mkMerge ([
+      {
+        home = {
+          packages = with pkgs; [
+            unstable.opencode
           ];
+
+          sessionVariables = {
+            OPENCODE_CONFIG = ./opencode.json;
+          };
         };
-      };
-    })
-  ];
+      }
+    ]
+    ++ (lib.optionals (builtins.hasAttr "persistence" options.home) [
+      {
+        home.persistence = {
+          "/persist" = {
+            directories = [
+              ".config/opencode"
+              ".cache/opencode"
+              ".local/share/opencode"
+              ".local/state/opencode"
+            ];
+          };
+        };
+      }
+    ]));
 }
