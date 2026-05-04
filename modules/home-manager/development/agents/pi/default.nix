70ae5314ae83d9a2f34e8b44faa346701f5c55b8
Rui Maciel <ruigouveiamaciel@proton.me>
2026-05-04 09:21:49 +0100

    chore(pi): bump pi-coding-agent to 0.72.1

--- modules/home-manager/development/agents/pi/default.nix
+++ modules/home-manager/development/agents/pi/default.nix
@@ -7,7 +7,7 @@
   config = lib.mkMerge ([
       {
         programs.fish.shellAliases = {
-          "pi" = "pnpx @mariozechner/pi-coding-agent@0.70.6";
+          "pi" = "pnpx @mariozechner/pi-coding-agent@0.72.1";
         };
         home = {
           activation = {
